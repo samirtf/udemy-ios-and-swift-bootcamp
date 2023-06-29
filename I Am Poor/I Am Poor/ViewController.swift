af9a5ef683603718e0484ce661cbe15634adbd60
samirtf <samirtf@outlook.com>
2023-06-29 20:40:05 -0300

    add I Am Poor project

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by SAMIR TRAJANO FEITOSA on 29/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
